91d31d22ce0a970eed88b0fe2b2180911887795f
idedeic1 <idedeic1@etf.unsa.ba>
2019-09-10 17:37:18 +0200

    Klasa Osoba

---
+++ src/ba/unsa/etf/rpr/Osoba.java
@@ -0,0 +1,39 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Date;
+
+public class Osoba {
+    private String ime;
+    private String prezime;
+    private Date datumRodjenja;
+
+    public Osoba(String ime, String prezime, Date datumRodjenja) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Date getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(Date datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+}
